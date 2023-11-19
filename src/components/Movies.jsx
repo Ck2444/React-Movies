b6e8551f4943347020abe93bc6499e3276063c32
Konstantin <t777tt88@gmai.com>
2023-11-19 12:26:58 +0100

    project complete

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,15 +1,18 @@
-import Movie from './Movie'
-
+import Movie from "./Movie";
 
 function Movies(props) {
-    const { movies } = props
-    return <div className="movies">
-        {movies.map(movie => {
-            return <Movie key={movie.imdbID} {...movie} />
-
-        })}
+  const { movies = [] } = props;
+  return (
+    <div className="movies">
+      {movies.length ? (
+        movies.map((movie) => {
+          return <Movie key={movie.imdbID} {...movie} />;
+        })
+      ) : (
+        <h4>Nothing found</h4>
+      )}
     </div>
-
+  );
 }
 
-export { Movies }
+export { Movies };
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
-    return <nav className='#43a047 green darken-1'>
-        <div classNameName="nav-wrapper">
-            <a href="#" className="brand-logo">React Movies</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
-            </ul>
-        </div>
+  return (
+    <nav className="#43a047 green darken-1">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          React Movies
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="!#">Repo</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-
+  );
 }
